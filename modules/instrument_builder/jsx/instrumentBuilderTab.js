e066f77f84cdd98165287e80a069e7800c95dcb0
zaliqarosli <zaliqa.rosli@mcin.ca>
2019-03-18 11:51:08 -0400

    start

---
+++ modules/instrument_builder/jsx/instrumentBuilderTab.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+// import PropTypes from 'prop-types';
+
+import Toolbar from './toolbar';
+
+class InstrumentBuilderTab extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Toolbar
+
+      />
+    );
+  }
+}
+
+InstrumentBuilderTab.propTypes = {
+
+};
+
+export default InstrumentBuilderTab;
---
+++ modules/instrument_builder/jsx/toolbar.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+// import PropTypes from 'prop-types';
+
+import Panel from 'Panel';
+
+class Toolbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Panel
+      >
+      <div id="sidebar-wrapper" className="sidebar-div">
+
+      </div>
+      </Panel>
+    );
+  }
+}
+
+Toolbar.propTypes = {
+
+};
+
+export default Toolbar;
