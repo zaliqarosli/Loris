e5da331d8c1b5e5a71b97a31b0740a12ece9d025
zaliqarosli <zaliqa.rosli@mcin.ca>
2019-03-25 15:42:27 -0400

    add more

--- modules/instrument_builder/jsx/instrumentBuilderTab.js
+++ modules/instrument_builder/jsx/instrumentBuilderTab.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 // import PropTypes from 'prop-types';
 
 import Toolbar from './toolbar';
+import Canvas from './canvas';
+import EditDrawer from './editdrawer';
 
 class InstrumentBuilderTab extends Component {
   constructor(props) {
@@ -17,10 +19,29 @@ class InstrumentBuilderTab extends Component {
   }
 
   render() {
-    return (
-      <Toolbar
+    const divStyle = {
+      border: '1px solid #C3D5DB',
+      borderRadius: '4px',
+      height: '678px',
+      marginTop: '-6px',
+      display: 'flex',
+      flexFlow: 'row nowrap',
+      alignItems: 'stretch',
+    };
 
-      />
+    return (
+      <div style={divStyle}>
+        <Toolbar
+        >
+        </Toolbar>
+        <Canvas
+        >
+        </Canvas>
+        <EditDrawer
+        >
+        </EditDrawer>
+
+      </div>
     );
   }
 }
