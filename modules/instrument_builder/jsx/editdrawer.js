367371985e922f1408d38e9cea10aa71c895083e
zaliqarosli <zaliqa.rosli@mcin.ca>
2019-06-18 18:09:48 -0400

    new ui changes

---
+++ modules/instrument_builder/jsx/editdrawer.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+// import PropTypes from 'prop-types';
+
+
+class EditDrawer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    const style = {
+      background: 'white',
+      border: '1px solid #C3D5DB',
+      order: 3,
+      flex: '1',
+      margin: '-1px -1px 0 -2px',
+    };
+
+    return (
+      <div style={style}>
+
+      </div>
+    );
+  }
+}
+
+EditDrawer.propTypes = {
+
+};
+
+export default EditDrawer;
